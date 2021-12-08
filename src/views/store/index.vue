9c3b7de9901324e5fcdfc62566085e8302325012
Ethan <zhennankee@gmail.com>
2021-12-08 15:38:11 +0800

    Update store management

--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -8,12 +8,43 @@
 
     <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
       <el-table-column type="selection" width="55" />
-      <el-table-column align="center" label="门店地址">
+      <el-table-column align="center" label="门店名称">
         <template slot-scope="{row}">
           <span>{{ row.name }}</span>
         </template>
       </el-table-column>
-
+      <el-table-column align="center" label="照片" width="120">
+        <template slot-scope="{row}">
+          <img :src="row.photo" alt="" style="width: 40px">
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="联系人" width="120">
+        <template slot-scope="{row}">
+          <span>{{ row.contact }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="手机" width="120">
+        <template slot-scope="{row}">
+          <span>{{ row.phone }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="门店地址" width="400">
+        <template slot-scope="{row}">
+          <span>{{ CodeToText[row.region[0]] + CodeToText[row.region[1]] + CodeToText[row.region[2]] + ', ' + row.address }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="营业时间" width="180">
+        <template slot-scope="{row}">
+          <span>{{ row.business_hours[0] + ' - ' + row.business_hours[1] }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="经营状态" width="100">
+        <template slot-scope="{row}">
+          <el-tag :type="row.status | statusType">
+            {{ row.status | statusFilter }}
+          </el-tag>
+        </template>
+      </el-table-column>
       <el-table-column align="center" label="操作" width="120">
         <template slot-scope="{row}">
           <el-button type="primary" size="mini" @click="editStore(row.id)">
@@ -23,10 +54,51 @@
       </el-table-column>
     </el-table>
 
-    <el-dialog title="门店管理" width="30%" :visible.sync="dialogFormVisible">
-      <el-form ref="couponForm" :rules="rules" :model="couponForm" label-position="left" label-width="110px" style="width: 80%; margin-left:50px;">
-        <el-form-item label="门店名称">
+    <el-dialog title="门店管理" width="700px" :visible.sync="dialogFormVisible">
+      <el-form ref="couponForm" :rules="rules" :model="couponForm" label-position="right" label-width="110px" style="width: 80%; margin-left:50px;">
+        <el-form-item label="门店名称" prop="name">
           <el-input v-model="couponForm.name" />
+        </el-form-item>
+        <el-form-item label="照片">
+          <el-upload
+            class="thumbnail-uploader"
+            action=""
+            :show-file-list="false"
+            :http-request="handleFile"
+            accept=".jpg, .jpeg, .png"
+          >
+            <img v-if="couponForm.photo" :src="couponForm.photo" alt="">
+            <i v-else class="el-icon-plus thumbnail-uploader-icon" />
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="联系人" prop="contact">
+          <el-input v-model="couponForm.contact" />
+        </el-form-item>
+        <el-form-item label="门店地址" prop="region">
+          <el-cascader v-model="couponForm.region" class="width-100" size="large" :options="regionOptions" />
+        </el-form-item>
+        <el-form-item prop="address">
+          <el-input v-model="couponForm.address" placeholder="请输入详细地址" style="margin-top: 10px" />
+        </el-form-item>
+        <el-form-item label="手机" prop="phone">
+          <el-input v-model="couponForm.phone" />
+        </el-form-item>
+        <el-form-item label="营业时间" prop="business_hours">
+          <el-time-picker
+            v-model="couponForm.business_hours"
+            is-range
+            value-format="HH:mm:ss"
+            range-separator="至"
+            start-placeholder="开始时间"
+            end-placeholder="结束时间"
+            placeholder="选择时间范围"
+            style="width: 100%;"
+          />
+        </el-form-item>
+        <el-form-item label="经营状态">
+          <el-radio-group v-model="couponForm.status" class="filter-item">
+            <el-radio v-for="(item, index) in statusOptions" :key="index" :label="index"> {{ item }} </el-radio>
+          </el-radio-group>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -39,17 +111,42 @@
 
 <script>
 import { fetchList, fetchStore, destroy, createStore, updateStore } from '@/api/store'
-import waves from '@/directive/waves' // waves directive
+import { image } from '@/api/upload'
+import waves from '@/directive/waves'
+import { regionData, CodeToText } from 'element-china-area-data' // waves directive
 const defaultStoreForm = {
-  id: null,
-  name: null
+  id: undefined,
+  name: null,
+  photo: null,
+  contact: null,
+  address: null,
+  phone: null,
+  business_hours: null,
+  status: 1
 }
 
 export default {
   name: 'StoreItems',
   directives: { waves },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        0: '未开启',
+        1: '已开启'
+      }
+      return statusMap[status]
+    },
+    statusType(status) {
+      const statusMap = {
+        0: 'info',
+        1: 'success'
+      }
+      return statusMap[status]
+    }
+  },
   data() {
     return {
+      CodeToText,
       id: null,
       list: null,
       listLoading: true,
@@ -58,8 +155,15 @@ export default {
       importDialogFormVisible: false,
       multipleSelection: [],
       couponForm: Object.assign({}, defaultStoreForm),
+      statusOptions: ['未开启', '已开启'],
+      regionOptions: regionData,
       rules: {
-        name: [{ required: true, message: '标题不可为空!', trigger: 'blur' }]
+        name: [{ required: true, message: '标题不可为空!', trigger: 'blur' }],
+        contact: [{ required: true, message: '联系人不可为空!', trigger: 'blur' }],
+        region: [{ required: true, message: '省市区不可为空!', trigger: 'change' }],
+        address: [{ required: true, message: '地址不可为空!', trigger: 'blur' }],
+        phone: [{ required: true, message: '手机不可为空!', trigger: 'blur' }],
+        business_hours: [{ required: true, message: '营业时间不可为空!', trigger: 'change' }]
       }
     }
   },
@@ -108,6 +212,13 @@ export default {
         }
       })
     },
+    handleFile(file) {
+      const formData = new FormData()
+      formData.set('file', file.file)
+      image(formData).then(response => {
+        this.couponForm.photo = response.data.path
+      })
+    },
     handleDestroy(id, index) {
       destroy(id).then(response => {
         this.$notify({
@@ -122,8 +233,7 @@ export default {
     editStore(id) {
       this.dialogFormVisible = true
       fetchStore(id).then(res => {
-        this.couponForm.id = res.data.id
-        this.couponForm.name = res.data.name
+        this.couponForm = res.data
       })
     },
     handleCreateStore() {
@@ -133,3 +243,31 @@ export default {
   }
 }
 </script>
+<style>
+.thumbnail-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.thumbnail-uploader .el-upload img {
+  width: 350px;
+}
+.thumbnail-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.thumbnail-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 300px;
+  height: 150px;
+  line-height: 150px;
+  text-align: center;
+}
+.thumbnail {
+  width: 300px;
+  height: 150px;
+  display: block;
+}
+</style>
